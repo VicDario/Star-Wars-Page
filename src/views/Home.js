9cba44411c460db90551cb4f4dcedd88721c4c92
VicDario <26585356+VicDario@users.noreply.github.com>
2021-06-17 21:24:25 -0400

    'species' and 'planets' sections added in home page. changes in scrollbar. sections now are 'carousel'

--- src/views/Home.js
+++ src/views/Home.js
@@ -5,25 +5,25 @@ import './Home.css';
 function Home (props) {
     
     const {store, actions} = useContext(Context);
-    const { people } = store;
+    const { people, species, planets } = store;
 
     return (
         <main className="container">
-            <div className="row">
+            <div className="row my-5">
                     <h2 className="text-white">Characters</h2>
+                    <div className="col-md-12 carousel">
                 {
                     !!people ?
                     (
                         people.results.map((character, key) => {
                         return (
-                            <div className="col-md-4 my-4" key={key}>
-                                <div className="card" >
+                                <div className="card my-4" key={key} >
                                     <img src="https://via.placeholder.com/200/300" className="card-img-top" alt="..." />
                                     <div className="card-body">
                                         <h4 className="card-text text-white">{character.name}</h4>
                                     </div>
                                 </div>
-                            </div>);
+                            );
                         })
                     ):(
                     <div className="d-flex justify-content-center">
@@ -33,6 +33,59 @@ function Home (props) {
                     </div>
                     )
                 }
+                    </div>
+            </div>
+            <div className="row my-5">
+                    <h2 className="text-white">Species</h2>
+                    <div className="col-md-12 carousel">
+                {
+                    !!species ?
+                    (
+                        species.results.map((specie, key) => {
+                        return (
+                                <div className="card my-4" key={key} >
+                                    <img src="https://via.placeholder.com/200/300" className="card-img-top" alt="..." />
+                                    <div className="card-body">
+                                        <h4 className="card-text text-white">{specie.name}</h4>
+                                    </div>
+                                </div>
+                            );
+                        })
+                    ):(
+                    <div className="d-flex justify-content-center">
+                        <div className="spinner-grow text-light" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                        </div>
+                    </div>
+                    )
+                }
+                    </div>
+            </div>
+            <div className="row my-5">
+                    <h2 className="text-white">planets</h2>
+                    <div className="col-md-12 carousel">
+                {
+                    !!planets ?
+                    (
+                        planets.results.map((planet, key) => {
+                        return (
+                                <div className="card my-4" key={key} >
+                                    <img src="https://via.placeholder.com/200/300" className="card-img-top" alt="..." />
+                                    <div className="card-body">
+                                        <h4 className="card-text text-white">{planet.name}</h4>
+                                    </div>
+                                </div>
+                            );
+                        })
+                    ):(
+                    <div className="d-flex justify-content-center">
+                        <div className="spinner-grow text-light" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                        </div>
+                    </div>
+                    )
+                }
+                    </div>
             </div>
         </main>
     );
