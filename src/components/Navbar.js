83389a9ff8dd4d374bd46f351313ab3e455e6bba
VicDario <26585356+VicDario@users.noreply.github.com>
2021-06-20 01:36:53 -0400

    Added dropdown in nav for favorites

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,20 @@ import {Link} from 'react-router-dom';
 
 function Navbar () {
     return (
-        <nav className="navbar navbar-dark bg-dark d-flex justify-content-center">
+        <nav className="navbar navbar-dark bg-dark d-flex justify-content-around">
             <Link to='/'>
             <img className="navbar__logo" src ={logo} alt="Logo" />
             </Link>
+            <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Favorites
+          </a>
+          <ul className="dropdown-menu bg-dark" aria-labelledby="navbarDropdown">
+            <li><a className="dropdown-item text-white" href="#">Action</a></li>
+            <li><a className="dropdown-item text-white" href="#">Another action</a></li>
+            <li><a className="dropdown-item text-white" href="#">Something else here</a></li>
+          </ul>
+        </li>
         </nav>
     );
 }
