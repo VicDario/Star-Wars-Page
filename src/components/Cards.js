6fc49d08a22c60d8f57d66393a0cfe16fb2a3071
VicDario <26585356+VicDario@users.noreply.github.com>
2021-06-28 19:29:06 -0400

    favicon changed, and some pictures added

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,6 +2,9 @@ import { Link } from 'react-router-dom';
 import './Cards.css'
 
 function Cards (props) {
+    const getImgName = name => {
+        return name.toLowerCase().split(" ").join("-") + ".jpg";
+    }
     return (
         !!props.elements &&
         props.elements.results.map((element, key) => {
@@ -9,7 +12,7 @@ function Cards (props) {
                 <div className="col-md-4 col-sm-6 my-4" key={key}>
                     <Link to={`${props.route}/${element.name.split(" ").join("").toLowerCase()}/${element.uid}`} >
                         <div className="card card-page">
-                            <img src="https://via.placeholder.com/200/300" className="card-img-top" alt="..." />
+                            <img src={`/img/${getImgName(element.name)}`} className="card-img-top" alt={element.name} />
                             <div className="card-body">
                                 <h4 className="card-text text-white">{element.name}</h4>
                             </div>
